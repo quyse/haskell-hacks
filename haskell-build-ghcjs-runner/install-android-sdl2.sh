034d0bc494de5336601fa9a8f9f32798807662cb
Quyse Lert <quyse0@gmail.com>
2017-04-27 23:36:25 +0300

    trying to add android ndk

---
+++ haskell-build-ghcjs-runner/install-android-sdl2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+export TOOLCHAIN=/opt/android-ndk-toolchain-arm
+export PATH=$TOOLCHAIN/bin:$PATH
+
+curl -L https://www.libsdl.org/release/SDL2-2.0.4.tar.gz | tar xzf -
+pushd SDL2-2.0.4
+
+CFLAGS='-std=gnu99 -fPIC' ./configure \
+	--host arm-linux-androideabi \
+	--prefix $TOOLCHAIN/sysroot/usr \
+	--disable-shared \
+	--disable-audio \
+	--disable-video-x11 \
+	--disable-video-wayland \
+	--disable-video-opengles1
+make
+make install
+popd
+rm -r SDL2-2.0.4
