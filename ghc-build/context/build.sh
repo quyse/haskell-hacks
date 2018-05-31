8b3cde72c90d91334b4016249c90e96bea3b533e
Quyse Lert <quyse0@gmail.com>
2018-05-31 08:38:53 +0300

    a bit of refactor of building script

--- ghc-build/context/build.sh
+++ ghc-build/context/build.sh
@@ -2,13 +2,15 @@
 
 set -e
 
+VERSION=8.4.2
+
 # get sources
 git config --global url."git://github.com/ghc/packages-".insteadOf     git://github.com/ghc/packages/
 git config --global url."http://github.com/ghc/packages-".insteadOf    http://github.com/ghc/packages/
 git config --global url."https://github.com/ghc/packages-".insteadOf   https://github.com/ghc/packages/
 git config --global url."ssh://git@github.com/ghc/packages-".insteadOf ssh://git@github.com/ghc/packages/
 git config --global url."git@github.com:/ghc/packages-".insteadOf      git@github.com:/ghc/packages/
-git clone https://github.com/ghc/ghc.git -b ghc-8.4.1-release --depth 1 --recursive
+git clone https://github.com/ghc/ghc.git -b ghc-$VERSION-release --depth 1 --recursive
 
 # configure and make
 pushd ghc
@@ -20,4 +22,4 @@ make -j$(nproc)
 make binary-dist
 popd
 
-cp ghc/ghc-8.4.1-*.tar.xz /data/ghc-8.4.1-integersimple.tar.xz
+cp ghc/ghc-$VERSION-*.tar.xz /data/ghc-$VERSION-integersimple.tar.xz
